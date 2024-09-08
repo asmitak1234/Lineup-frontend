8fb6de35ec678d789efb52196451eca8468f5785
ASMITA KUMARI <166974264+asmitak1234@users.noreply.github.com>
2024-09-08 17:17:23 +0530

    host

--- src/pages/CustomNavbar.jsx
+++ src/pages/CustomNavbar.jsx
@@ -8,11 +8,11 @@ export default function CustomNavbar({ formContent }) {
     return (
         <Navbar className="mb-2" expand="lg" bg="dark" variant="dark" style={{height:"10vh",}}>
             <Container>
-                <Navbar.Brand style={{fontSize:"38px",}}>LineUp</Navbar.Brand>
-                <Navbar.Toggle />
-                <Navbar.Collapse className="justify-content-end">
+                <Navbar.Brand style={{fontSize:"38px",}}><strong className="mx-1">LineUp</strong></Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id='basic-navbar-nav' className="justify-content-end">
                     <Navbar.Text>
-                        <form style={{display:"inline",}} onSubmit={e => formContent.onSubmit(e)}>
+                        <form className='my-2 d-inline' style={{display:"inline",}} onSubmit={e => formContent.onSubmit(e)}>
                             {formContent.content}
                         </form>
                     </Navbar.Text>
